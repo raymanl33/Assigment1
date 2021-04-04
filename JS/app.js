3bf902a34b0e26559a5c2dcc33a9779c853c8b66
Raymond Lee <raymanl33@icloud.com>
2021-04-04 12:01:46 -0700

    add textarea, save, and delete button when "create a new note button" is clicked in index.html

--- JS/app.js
+++ JS/app.js
@@ -13,45 +13,112 @@ function change_theme() {
         document.querySelector('.container').classList.add("ChangeTheme")
         let toggleStatus = document.querySelector(".ChangeTheme").classList.toggle('.container')
         if (toggleStatus === false) {
-            document.querySelector('.ChangeTheme').style.backgroundColor = '#f2e9de'
+            document.querySelector('.ChangeTheme').style.opacity = ""
             document.querySelector('p').style.backgroundColor = '#f2e9de'
             document.querySelector('footer').style.backgroundColor = '#f2e9de'
             document.querySelector('.note_container').style.backgroundColor = '#f2e9de'
             document.querySelector('.header').style.backgroundColor = '#f2e9de'
-            document.querySelector('.note').style.backgroundColor = '#f2e9de'
-            document.querySelector('.note_button').style.backgroundColor = '#f2e9de'
-            document.querySelector('.delete').style.backgroundColor = '#f2e9de'
-            document.querySelector('.save').style.backgroundColor = '#f2e9de'
-            document.querySelector('.save_delete').style.backgroundColor = '#f2e9de'
+            // document.querySelector('.note').style.backgroundColor = '#f2e9de'
+            // document.querySelector('.note_button').style.backgroundColor = '#f2e9de'
+            // document.querySelector('.delete').style.backgroundColor = '#f2e9de'
+            // document.querySelector('.save').style.backgroundColor = '#f2e9de'
+            // document.querySelector('.save_delete').style.backgroundColor = '#f2e9de'
             document.querySelector('ul').style.backgroundColor = '#f2e9de'
             document.querySelector('a').style.backgroundColor = '#f2e9de'
             document.querySelector('.navigation').style.backgroundColor = '#f2e9de'
             document.querySelectorAll(".Navigation")[1].style.backgroundColor = '#f2e9de'
             document.querySelectorAll(".Navigation a")[1].style.backgroundColor = '#f2e9de'
+            document.querySelector('.create_button').style.backgroundColor = '#f2e9de'
             console.log(toggleStatus)
         } else {
-            console.log(toggleStatus)
+            document.querySelector('body').style.backgroundColor = '#f2e9de'
             document.querySelector('p').style.backgroundColor = '#555169'
             document.querySelector('footer').style.backgroundColor = '#555169'
             document.querySelector('.note_container').style.backgroundColor = '#555169'
             document.querySelector('.header').style.backgroundColor = '#555169'
-            document.querySelector('.note').style.backgroundColor = '#555169'
-            document.querySelector('.note_button').style.backgroundColor = '#555169'
-            document.querySelector('.delete').style.backgroundColor = '#555169'
-            document.querySelector('.save').style.backgroundColor = '#555169'
-            document.querySelector('.save_delete').style.backgroundColor = '#555169'
+            // document.querySelector('.note').style.backgroundColor = '#555169'
+            // document.querySelector('.note_button').style.backgroundColor = '#555169'
+            // document.querySelector('.delete').style.backgroundColor = '#555169'
+            // document.querySelector('.save').style.backgroundColor = '#555169'
+            // document.querySelector('.save_delete').style.backgroundColor = '#555169'
             document.querySelector('ul').style.backgroundColor = '#555169'
             document.querySelector('a').style.backgroundColor = '#555169'
             document.querySelector('.navigation').style.backgroundColor = '#555169'
             document.querySelector('li a').style.backgroundColor = '#555169'
             document.querySelectorAll(".Navigation")[1].style.backgroundColor = '#555169'
             document.querySelectorAll(".Navigation a")[1].style.backgroundColor = '#555169'
+            document.querySelector('.create_button').style.backgroundColor = '#555169'
+            console.log(toggleStatus)
         }
     })
 }
 
 change_theme();
 
+// let list = document.querySelector('ul');  < ----
+// list.insertAdjacentHTML('beforebegin', '<p>heelo</P>');
+
+function new_note () {
+    let create_note = document.querySelector('.create_button')
+    create_note.classList.add('create_note')
+    let toggleStatus = document.querySelector(".create_note").classList.toggle('.create_button')
+    if (toggleStatus === false) {
+        let new_row = document.createElement('div');
+        new_row.classList.add("note");
+
+        let createButton = document.createElement('button');
+        createButton.innerText = '+';
+        createButton.setAttribute("id", "note_button");
+        create_note.parentNode.replaceChild(createButton, create_note);
+        let parent = document.querySelector('#note_button')
+
+        let newNote = document.createElement('textarea');
+        newNote.cols = "40";
+        newNote.rows = "20";
+        newNote.innerText = 'your note here';
+        // create_note.parentNode.replaceChild(newNote, create_note);
+        newNote.setAttribute("id", "story");
+        document.getElementById("note_button").appendChild(newNote);
+        
+        // let saveButton = document.createElement('button');
+        // saveButton.innerText = 'save';
+        // saveButton.className = 'save';
+        parent.insertAdjacentHTML('beforeend', '<button class="save" type="button"> save</button>');
+        parent.insertAdjacentHTML('beforeend', '<button class="delete" type="button"> delete</button>');
+        // let note = document.createElement("textarea");
+        // note.name = "post";
+        // note.maxLength = "5000";
+        // note.cols = "40";
+        // note.rows = "15";
+        // note.style.backgroundColor = '#e0def4';
+        
+        // document.body.appendChild(note);
+        // document.querySelector('.create_button').innerHTML = "your note here";
+        console.log(toggleStatus)
+    }
+    else {
+        document.querySelector('.create_button').innerHTML = "+ create a new note";
+        document.querySelector('textarea').remove();
+    }
+   
+    //if new note is clicked:    
+        //clicking on the new note button in index.html should open
+        //a note taking area along with a save and cancel buttom
+            //if save is clicked
+                
+                // should remove the note taking area and buttons and
+                    //return the title and body of the note to push 
+                    // make sure to return (object)
+                    //to new_notes function  
+            //if delete is clicked 
+                //text area should close without saving the note
+
+}
+
+
+new_note();  // <---- figure out how to target .create_button
+
+// let note = document.getElementById("#story").value;   <-- can use this to read the value 
 
 // - use document.querySelectorAll("li") to access the stored notesArray
 function store_notes (save_note, note1or2) {
@@ -75,23 +142,7 @@ function store_notes (save_note, note1or2) {
 
 store_notes(save_note, note1or2); // <----- note1or2 is talking about the li in the sidebar
 
-function new_notes (create_button) {
 
-            //if new note is clicked:    
-                //clicking on the new note button in index.html should open
-                //a note taking area along with a save and cancel buttom
-                    //if save is clicked
-                        // should remove the note taking area and buttons and
-                            //return the title and body of thenote to push 
-                            // make sure to return (object)
-                            //to new_notes function  
-                    //if delete is clicked 
-                        //text area should close without saving the note
-
-}
-
-
-new_notes(create_button);  // <---- figure out how to target .create_button
 
 
 
